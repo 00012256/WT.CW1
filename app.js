ec29a1a9b07043d279a6240e14ceda519bafa509
00012256 <id00012256@gmail.com>
2021-12-05 20:42:20 +0500

    Finished homepage

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
+// back to top button
+window.addEventListener('scroll', function() {
+  var scroll = document.querySelector('.scrollTop')
+  scroll.classList.toggle("active", window.scrollY > 500)
+})
+
+document.getElementById('scrollTop').addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+});
 
 // add active class in smaller screens
+hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
+// if a nav link or hamburger menu is clicked close the hamburger menu
 navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-// if a nav link is clicked close the hamburger menu
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// add active-link class to current page
+const activePage = window.location.pathname;
+navLink.forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active-link');
+    console.log(link);
+  }
+})
